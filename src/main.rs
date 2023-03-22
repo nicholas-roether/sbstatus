8160ce7e1174cd4d668ba27130546e34fe0f9255
nicholasroether4308 <nicholas.roether@otto.de>
2023-03-22 16:11:50 +0100

    Add mpris support

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::{thread, time::Duration};
+
+use system::player::PlayerDataCollector;
+
+mod system;
+
 fn main() {
-    println!("Hello, world!");
+    let mut data_fetcher = PlayerDataCollector::new().unwrap();
+
+    loop {
+        data_fetcher.refresh();
+        println!("{:?}", data_fetcher.player_data());
+        thread::sleep(Duration::from_millis(200));
+    }
 }
---
+++ src/system/player.rs
@@ -0,0 +1,80 @@
+use mpris::{DBusError, Metadata, PlaybackStatus, Player, PlayerFinder};
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct RunningPlayerData {
+    pub title: String,
+    pub artist: Option<String>,
+    pub playing: bool,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum PlayerData {
+    Stopped,
+    Running(RunningPlayerData),
+}
+
+pub struct PlayerDataCollector {
+    player_finder: PlayerFinder,
+    active_player: Option<Player>,
+    player_data: Option<PlayerData>,
+}
+
+impl PlayerDataCollector {
+    pub fn new() -> Result<Self, DBusError> {
+        Ok(Self {
+            player_finder: PlayerFinder::new()?,
+            active_player: None,
+            player_data: None,
+        })
+    }
+
+    pub fn refresh(&mut self) {
+        self.refresh_active_player();
+        self.refresh_player_data();
+    }
+
+    pub fn player_data(&self) -> &'_ Option<PlayerData> {
+        &self.player_data
+    }
+
+    fn refresh_active_player(&mut self) {
+        if let Some(player) = &self.active_player {
+            if !player.is_running() {
+                self.active_player = None;
+            }
+        }
+        if self.active_player.is_none() {
+            if let Ok(new_active) = self.player_finder.find_active() {
+                self.active_player = Some(new_active);
+            }
+        }
+    }
+
+    fn fetch_new_player_data(player: &Player) -> Option<PlayerData> {
+        let Ok(meta) = player.get_metadata() else {
+            return None;
+        };
+
+        let Ok(status) = player.get_playback_status() else {
+            return None;
+        };
+
+        if status == PlaybackStatus::Stopped {
+            return Some(PlayerData::Stopped);
+        }
+
+        Some(PlayerData::Running(RunningPlayerData {
+            title: meta.title().unwrap_or("<Unknown Title>").to_string(),
+            artist: meta.artists().map(|strs| strs.join(", ")),
+            playing: status == PlaybackStatus::Playing,
+        }))
+    }
+
+    fn refresh_player_data(&mut self) {
+        self.player_data = if let Some(player) = &self.active_player {
+            Self::fetch_new_player_data(player)
+        } else {
+            None
+        }
+    }
+}
